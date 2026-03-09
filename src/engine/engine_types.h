a8c9c20871cb4f8f78ee6939379d527b9a463be1
Anna Udobnaja <udobnaja@gmail.com>
2026-03-09 15:08:29 +0200

    C3D-5: draft contract for interactions

--- src/engine/engine_types.h
+++ src/engine/engine_types.h
@@ -4,12 +4,19 @@
 #include "types.h"
 #include "textures_types.h"
 
+#define X11_KEY_PRESS_EVENT 2
+#define X11_KEY_RELEASE_EVENT 3
+#define X11_KEY_PRESS_MASK (1L << 0)
+#define X11_KEY_RELEASE_MASK (1L << 1)
+#define X11_DESTROY_NOTIFY_EVENT 17
+
 typedef struct s_engine_events
 {
-	int		(*on_key_down)(int keycode, void *userdata);
-	int		(*on_close)(void *userdata);
-	int		(*on_tick)(void *userdata);
-	void	*userdata;
+	int		(*on_key_down)(int keycode, void *data);
+    int		(*on_key_up)(int keycode, void *data);
+	int		(*on_close)(void *data);
+	int		(*on_tick)(void *data);
+	void	*data;
 }	t_engine_events;
 
 typedef struct s_engine
--- test.c
+++ test.c
@@ -8,92 +8,128 @@
 #define SIZE 10 // TODO: delete
 
 
+
+int handle_key_down(int keycode, void *data)
+{
+	(void) keycode;
+	(void) data;
+	return (0);
+}
+
+int handle_key_up(int keycode, void *data){
+	(void) keycode;
+	(void) data;
+	return (0);
+}
+
+
+int handle_close(void *data)
+{
+	(void) data;
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
+int handle_game_tick(void *data)
+{
+	(void) data;
+	return (0);
+}
+	
+
 int	main(int argc, char **argv)
 {
-    (void) argc;
-    (void) argv;
+	(void) argc;
+	(void) argv;
 
-    // Init Game
-    // Validate Input
-    // Validate Maps
-    // Parse Input & Init Game
-    // Init Graphics
-    // Render
-    // Hooks
-    // Mlx_loop
+	// Init Game
+	// Validate Input
+	// Validate Maps
+	// Parse Input & Init Game
+	// Init Graphics
+	// Render
+	// Hooks
+	// Mlx_loop
 
-    t_game	game;
+	t_game	game;
 
 	ft_bzero(&game, sizeof(t_game));
-     static char *mock_map[SIZE] = {
-        "1111111111",
-        "1000000001",
-        "1000000001",
-        "1000010001",
-        "1000010001",
-        "1000010001",
-        "1000010001",
-        "1000000001",
-        "1000000001",
-        "1111111111"
-    };
+	 static char *mock_map[SIZE] = {
+		"1111111111",
+		"1000000001",
+		"1000000001",
+		"1000010001",
+		"1000010001",
+		"1000010001",
+		"1000010001",
+		"1000000001",
+		"1000000001",
+		"1111111111"
+	};
 
-    // INIT_SCENE
-    // Mock info
-    game.scene.map_size.height = SIZE;
-    game.scene.map_size.width = SIZE;
+	// INIT_SCENE
+	// Mock info
+	game.scene.map_size.height = SIZE;
+	game.scene.map_size.width = SIZE;
 
-    game.scene.player.pos.x = 2.0;
-    game.scene.player.pos.y = 2.0;
+	game.scene.player.pos.x = 2.0;
+	game.scene.player.pos.y = 2.0;
 
-    game.scene.player.angle = 0.0;
+	game.scene.player.angle = 0.0;
 
-    game.scene.palette.ceiling = 0x202030;
-    game.scene.palette.floor = 0x151515;
+	game.scene.palette.ceiling = 0x202030;
+	game.scene.palette.floor = 0x151515;
 
-    game.scene.map = ft_calloc(game.scene.map_size.height + 1, sizeof(char *));
-    if (!game.scene.map)
-        return (1); // TODO: error handling
-    int i = 0;
-    for (i = 0; i < game.scene.map_size.height; i++) {
-        game.scene.map[i] = ft_strdup(mock_map[i]);
-        if (!game.scene.map[i])
-            return (1); // TODO: error handling + cleanup
-    }   
+	game.scene.map = ft_calloc(game.scene.map_size.height + 1, sizeof(char *));
+	if (!game.scene.map)
+		return (1); // TODO: error handling
+	int i = 0;
+	for (i = 0; i < game.scene.map_size.height; i++) {
+		game.scene.map[i] = ft_strdup(mock_map[i]);
+		if (!game.scene.map[i])
+			return (1); // TODO: error handling + cleanup
+	}   
 
-    game.scene.map[game.scene.map_size.height] = NULL;
+	game.scene.map[game.scene.map_size.height] = NULL;
 
-    game.scene.camera.fov = deg_to_rad(FOV);
-    game.scene.camera.scale = tan(game.scene.camera.fov / 2);
+	game.scene.camera.fov = deg_to_rad(FOV);
+	game.scene.camera.scale = tan(game.scene.camera.fov / 2);
 
-    // INIT ENGINE
-    game.engine.window_size = (t_dimensions){
-        .width = MAX_WIN_WIDTH,
-        .height = MAX_WIN_HEIGHT
-    };
-    if (engine_init(&game.engine, GAME_TITLE) != ENGINE_OK) {
-        // TODO: clean other sources
-        return (1);
-    }
+	// INIT ENGINE
+	game.engine.window_size = (t_dimensions){
+		.width = MAX_WIN_WIDTH,
+		.height = MAX_WIN_HEIGHT
+	};
 
-    // LOAD TEXTURES
-    const t_wall_texture_paths paths = (t_wall_texture_paths) {
-        .east =  "resources/ea.xpm",
-        .north = "resources/no.xpm",
-        .south = "resources/so.xpm", 
-        .west = "resources/we.xpm"
-    };
+	if (engine_init(&game.engine, GAME_TITLE) != ENGINE_OK) {
+		// TODO: clean other sources
+		return (1);
+	}
 
-    if (textures_load(&game.textures, game.engine.mlx_session, &paths) != TEX_OK) {
-        engine_shutdown(&game.engine);
-        return (1);
-    }
-    render_scene(&game.scene, game.engine.window_size, &game.engine.buffer.px, &game.textures);
-    mlx_put_image_to_window(game.engine.mlx_session,
-                        game.engine.mlx_window,
-                        game.engine.buffer.img,
-                        0, 0);
-    engine_run(&game.engine);
+	game.engine.events.data = NULL;
+	game.engine.events.on_close = handle_close;
+	game.engine.events.on_key_down = handle_key_down;
+	game.engine.events.on_key_up = handle_key_up;
+	game.engine.events.on_tick = handle_game_tick;
 
-    return (0);
+	// LOAD TEXTURES
+	const t_wall_texture_paths paths = (t_wall_texture_paths) {
+		.east =  "resources/ea.xpm",
+		.north = "resources/no.xpm",
+		.south = "resources/so.xpm", 
+		.west = "resources/we.xpm"
+	};
+
+	if (textures_load(&game.textures, game.engine.mlx_session, &paths) != TEX_OK) {
+		engine_shutdown(&game.engine);
+		return (1);
+	}
+	render_scene(&game.scene, game.engine.window_size, &game.engine.buffer.px, &game.textures);
+	mlx_put_image_to_window(game.engine.mlx_session,
+						game.engine.mlx_window,
+						game.engine.buffer.img,
+						0, 0);
+	engine_run(&game.engine);
+
+	return (0);
 }
