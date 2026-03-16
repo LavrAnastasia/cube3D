beb9d6a2c04d1f740afad6c04fd24983b387b6b3
Anna Udobnaja <udobnaja@gmail.com>
2026-03-16 12:51:54 +0200

    C3D-21: smelly norminette

--- src/pixels/index.c
+++ src/pixels/index.c
@@ -1,15 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audobnai <audobnai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 12:50:13 by audobnai          #+#    #+#             */
+/*   Updated: 2026/03/16 12:51:37 by audobnai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "textures_types.h"
 
 void	pixels_put(t_px_buffer *buffer, int x, int y, t_color color)
 {
-	char *dst;
-    
-    dst = buffer->data + (y * buffer->line_len + x * (buffer->bpp / 8));
+	char	*dst;
+
+	dst = buffer->data + (y * buffer->line_len + x * (buffer->bpp / 8));
 	*(t_color *)dst = color;
 }
 
-
-t_color pixels_get(const t_px_buffer *buffer, int x, int y)
+t_color	pixels_get(const t_px_buffer *buffer, int x, int y)
 {
 	t_color		color;
 	const int	i = y * buffer->line_len + x * (buffer->bpp / 8);
--- src/pixels/pixels.h
+++ src/pixels/pixels.h
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pixels.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audobnai <audobnai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 12:50:20 by audobnai          #+#    #+#             */
+/*   Updated: 2026/03/16 12:50:21 by audobnai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIXELS_H
 # define PIXELS_H
 
-#include "textures_types.h"
+# include "textures_types.h"
 
 void	pixels_put(t_px_buffer *px, int x, int y, t_color color);
 t_color	pixels_get(const t_px_buffer *buffer, int x, int y);
