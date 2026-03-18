9704a268b4347597349173a8c985c490bc3e84da
Anna Udobnaja <udobnaja@gmail.com>
2026-03-18 20:47:10 +0200

    C3D-19: textures headers

--- src/textures/textures_destroy.c
+++ src/textures/textures_destroy.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   textures_destroy.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audobnai <audobnai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 20:46:38 by audobnai          #+#    #+#             */
+/*   Updated: 2026/03/18 20:46:39 by audobnai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 # include "textures_internal.h"
 
 void texture_destroy(void *mlx_session, t_image_buffer *buffer)
