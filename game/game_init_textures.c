e4eff46409f7c194c16f95d4487d1f5f8bc8daa8
Anna Udobnaja <udobnaja@gmail.com>
2026-03-18 20:52:59 +0200

    C3D-19: game header

--- game/game_init_textures.c
+++ game/game_init_textures.c
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_init_textures.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audobnai <audobnai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 20:51:11 by audobnai          #+#    #+#             */
+/*   Updated: 2026/03/18 20:52:01 by audobnai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "game.h"
-#include "parsing.h"
+#include "config.h"
 
 bool game_init_textures(t_textures *textures, t_configuration *configuration, void *mlx_session)
 {
